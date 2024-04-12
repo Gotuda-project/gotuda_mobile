cdbafa2477612f3f936c8e0309d369fb49b97058
Kate Grigorenko <katyagr2002@gmail.com>
2024-04-12 17:53:40 +0300

    GOTUDA-24: Добавлена верстка события

---
+++ gotuda/gotuda/Utils/UIColor+Additions.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension Color {
+    public static let linearGradient1 = Color(
+        UIColor(red: 167 / 255, green: 255 / 255, blue: 129 / 255, alpha: 1))
+    public static let linearGradient2 = Color(UIColor(red: 40 / 255, green: 229 / 255, blue: 161 / 255, alpha: 1))
+    public static let lightGray = Color(UIColor(red: 238 / 255, green: 238 / 255, blue: 238 / 255, alpha: 1))
+}
